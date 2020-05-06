913f06c10ebef3ff27b48c903a25e2c4bee0a4f7
dragondungeon1 <lennartderidder@gmail.com>
2020-05-06 13:16:52 +0200

    added a navbar and a background color

--- index.php
+++ index.php
@@ -5,12 +5,18 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <title>Hotel Cali</title>
 </head>
-<body>
+<body class="bg">
 <div>
-    <nav>
-        test1
+    <nav class="menu">
+        <ul class="menu__list">
+            <li class="menu__group"><a href="index.php" class="menu__link">Home</a></li>
+            <li class="menu__group"><a href="#" class="menu__link">Rooms </a></li>
+            <li class="menu__group"><a href="#" class="menu__link">Services</a></li>
+            <li class="menu__group"><a href="#" class="menu__link">Locations</a></li>
+        </ul>
     </nav>
 </div>
 
