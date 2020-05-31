6e2d55a95eb6609900f34f3cae869d21fdbaf938
dragondungeon1 <lennartderidder@gmail.com>
2020-05-31 23:01:37 +0200

    fixed the product cards in admin++/index.php and I am building an admin panel -> AP.php.

---
+++ admin++/AP.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/stylesheet.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <div>
+        <nav class="menu">
+            <ul class="menu__list">
+                <img class="img-test" src="../img/logo2.jpg" alt="logo">
+                <li class="menu__group"><a href="#" class="menu__link">Hotel California</a></li>
+                <li class="menu__group"><a href="../index.php" class="menu__link">Home</a></li>
+                <li class="menu__group"><a href="../rooms.php" class="menu__link">Rooms </a></li>
+                <li class="menu__group"><a href="../accommodations.php" class="menu__link">Accommodations</a></li>
+                <li class="menu__group"><a href="#" class="menu__link">Locations</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="center">
+        <h2>Welcome to the Admin Panel. </h2>
+    </div>
+
+    <div class="center">
+       <p>Choose your option:</p>
+    </div>
+
+    <br> <br>
+
+    <div class="flex">
+        <div class="card" style="border-radius: 10px; background-color: ;">
+            <h1>Book a room</h1>
+            <p class="price">Book a room here</p>
+            <p><button class="button-warning" style="width: 95%;">Book room</button></p>
+        </div>
+
+        <div class="card" style="border-radius: 10px; background-color: ">
+            <h1>Room plan</h1>
+            <p class="price">Locations of the rooms</p>
+            <p><button class="button-danger" style="width: 95%;">Map</button></p>
+        </div>
+
+        <div class="card" style="border-radius: 10px; background-color: ">
+            <h1>Book a room</h1>
+            <p class="price">Book a room here</p>
+            <p><button class="button-succes" style="width: 95%;">Book room</button></p>
+        </div>
+
+        <div class="card" style="border-radius: 10px; background-color: ">
+            <h1>Book a room</h1>
+            <p class="price">Book a room here</p>
+            <p><button class="button-primary" style="width: 95%;">Book room</button></p>
+        </div>
+    </div>
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
